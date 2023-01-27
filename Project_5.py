9d2d62b5dc54495351f69e168824001f2c402373
Mahdi <123685546+MahdiUK@users.noreply.github.com>
2023-01-27 23:37:40 +0000

    Add files via upload

---
+++ Project_5.py
@@ -0,0 +1,9 @@
+age = int(input('Hi Enter Your Age: '))
+month = age * 12
+week = month * 4
+day = week * 7
+hour = day * 24
+minute = hour * 60
+second = minute * 60
+
+print (f' Your lifespan by month is = {month} months \n Your lifespan by week is = {week} weeks \n Your lifespan by day is = {day} days \n Your lifespan by hour is = {hour} hours \n Your lifespan by minute is = {minute} minutes \n Your lifespan by second is = {second} seconds \n ')
